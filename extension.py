41686df24609477e37f8f08062f731440f373ae2
2021113433 <106805163+2021113433@users.noreply.github.com>
2023-12-16 03:20:11 +0800

    First Part of Initial Report Done

--- extension.py
+++ extension.py
@@ -1,11 +1,14 @@
 #####################################################################
-# Imports
+# Imports and setups
 #####################################################################
 
 import os
+import tempfile
 from customtkinter import filedialog
 from tkinter import messagebox
 from datetime import datetime
+
+messages = []  
 
 #####################################################################
 # Get Directory
@@ -22,7 +25,7 @@ def getDirectory():
     rootDirectory_temp = filedialog.askdirectory()
 
     dateName_temp = datetime.now().strftime("%Y_%m_%d_%H_%M_%S")
-    folderName = "LinXtrac - Linux Extractions Systems Result"
+    folderName = "LinXtrac - Linux Extraction System Result"
 
     combinedName = os.path.join(currentWorkingDirectory_temp,dateName_temp)
     os.mkdir(combinedName)
@@ -44,38 +47,132 @@ def getDirectory():
 # Report
 #####################################################################
 
-def generateReport(OSInfo):
+def generateReport(osi, host, time):
 
     reportPath = os.path.join(mainFolder, "LinXtrac Report.html")
     with open(reportPath, "w") as reportFile:
-        # Write HTML markup
+
         reportFile.write("<html>\n")
         reportFile.write("<head>\n")
-        reportFile.write("<title>HTML Report</title>\n")
+        reportFile.write("<title>LinXtrac - Linux Extraction System Report</title>\n")
+        reportFile.write("<h1>LinXtrac</h1>\n")
+        reportFile.write("<h2>Linux Extractions System Report</h2>\n")
         reportFile.write("</head>\n")
         reportFile.write("<body>\n")
         
-        # Write own data
-        reportFile.write("<h1>Report Data</h1>\n")
-        reportFile.write("<p>{}</p>\n".format(OSInfo))
-        
-        # Close HTML markup
-        reportFile.write("</body>\n")
-        reportFile.write("</html>\n")
+        reportFile.write("<h3>OS Information</h3>\n")
+        reportFile.write("<p>OS: {}<br>Host: {}<br>Time: {}</p>\n".format(osi, host, time))
 
 def appendReport(data):
 
-    reportPath = os.path.join(mainFolder, "report.html")
+    reportPath = os.path.join(mainFolder, "LinXtrac Report.html")
     with open(reportPath, "a") as reportFile:
-        # Append data to existing HTML report
+        
         reportFile.write("<h1>Appended Data</h1>\n")
         reportFile.write("<p>{}</p>\n".format(data))
 
 def closeReport():
 
-    reportPath = os.path.join(mainFolder, "report.html")
+    reportPath = os.path.join(mainFolder, "LinXtrac Report.html")
     with open(reportPath, "a") as reportFile:
         reportFile.write("</body>\n")
         reportFile.write("</html>\n")
+
+    return
+
+#####################################################################
+# OS Information
+#####################################################################
+
+def getInfo():
+
+    usrDirectory = "usr"
+    libDirectory = "lib"
+    etcDirectory = "etc"
+    osToSearch = "os-release"
+    hostToSearch = "hostname"
+    timeToSearch = "localtime"
+
+    ### os-release
+
+    for relPath,dirs,files in os.walk(rootDirectory):
+        if(usrDirectory in dirs):
+            firstLayer = os.path.join(rootDirectory,relPath,usrDirectory)
+            break
+
+    for relPath,dirs,files in os.walk(firstLayer):
+        if(libDirectory in dirs):
+            targetedLayer = os.path.join(firstLayer,relPath,libDirectory)
+            break
+    
+    for relPath,dirs,files in os.walk(targetedLayer):
+        if(osToSearch in files):
+            osPath = os.path.join(targetedLayer,relPath,osToSearch)
+            break
+        else:
+            osPath = False
+    
+    osp = tempfile.NamedTemporaryFile(delete=False)
+    if osPath:
+        with open(osPath, "r") as input:
+            for line in input:
+                osp.write(line.encode())
+    osp.close()
+    
+    ### hostname
+
+    for relPath,dirs,files in os.walk(rootDirectory):
+        if(etcDirectory in dirs):
+            targetDirectory = os.path.join(rootDirectory,relPath,etcDirectory)
+            break
+    
+    for relPath,dirs,files in os.walk(targetDirectory):
+        if(hostToSearch in files):
+            hostPath = os.path.join(targetDirectory,relPath,hostToSearch)
+            break
+        else:
+            hostPath = False
+
+    hst = tempfile.NamedTemporaryFile(delete=False)
+    if hostPath:
+        with open(hostPath, "r") as input:
+            for line in input:
+                hst.write(line.encode())
+    hst.close()
+
+    ### zoneinfo
+
+    for relPath,dirs,files in os.walk(rootDirectory):
+        if(etcDirectory in dirs):
+            timeDirectory = os.path.join(rootDirectory,relPath,etcDirectory)
+            break
+    
+    for relPath,dirs,files in os.walk(timeDirectory):
+        if(timeToSearch in files):
+            timePath = os.path.join(timeDirectory,relPath,timeToSearch)
+            break
+        else:
+            timePath = False
+
+    znf = tempfile.NamedTemporaryFile(delete=False)
+    if timePath:
+        with open(timePath, "r") as input:
+            for line in input:
+                znf.write(line.encode())
+    znf.close()
+
+    ###
+
+    with open(osp.name, 'r') as file:
+        os_data = file.read()
+    with open(hst.name, 'r') as file:
+        host_data = file.read()
+    with open(znf.name, 'r') as file:
+        time_data = file.read()
+
+    generateReport(os_data, host_data, time_data)
+    os.remove(osp.name)
+    os.remove(hst.name)
+    os.remove(znf.name)
 
     return
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 #####################################################################
-# Imports
+# Imports and setups
 #####################################################################
 
 import customtkinter
@@ -15,7 +15,7 @@ customtkinter.set_default_color_theme("dark-blue")
 app = customtkinter.CTk()
 app.geometry("800x600")
 app.resizable(False, False)
-app.title("LinXtrac - Linux Extractions Systems")
+app.title("LinXtrac - Linux Extraction System")
 
 ###
 
@@ -24,7 +24,7 @@ mainFrame.pack(pady=20, padx=60, fill="both", expand=True)
 
 title = customtkinter.CTkLabel(master=mainFrame, text="\n\n\nLinXtrac", font=("Helvetica", 30))
 title.pack()
-subTitle = customtkinter.CTkLabel(master=mainFrame, text="\nLinux Extraction Systems", font=("Helvetica", 20))
+subTitle = customtkinter.CTkLabel(master=mainFrame, text="\nLinux Extraction System", font=("Helvetica", 20))
 subTitle.pack()
 flavourText = customtkinter.CTkLabel(master=mainFrame, text="Discover the power of efficient forensic investigation with LinXtrac", font=("Helvetica", 16, "italic"))
 flavourText.pack()
@@ -36,7 +36,7 @@ subFrame.pack(padx=80, pady=80)
 
 directoryText = customtkinter.CTkLabel(master=subFrame, text="No directory chosen.", font=("Helvetica", 15))
 directoryText.pack()
-directoryButton = customtkinter.CTkButton(master=subFrame, text="Choose Directory", command=lambda: [getDirectory(), mainFrame.pack_forget(), menuFrame.pack(pady=20, padx=60, fill="both", expand=True)])
+directoryButton = customtkinter.CTkButton(master=subFrame, text="Choose Directory", command=lambda: [getDirectory(),getInfo(), mainFrame.pack_forget(), menuFrame.pack(pady=20, padx=60, fill="both", expand=True)])
 directoryButton.pack(padx=10, pady=10)
 
 ###
@@ -54,7 +54,14 @@ menuFrame = customtkinter.CTkFrame(master=app)
 
 title = customtkinter.CTkLabel(master=menuFrame, text="\n\n\nLinXtrac", font=("Helvetica", 30))
 title.pack()
-subTitle = customtkinter.CTkLabel(master=menuFrame, text="\nLinux Extraction Systems", font=("Helvetica", 20))
+subTitle = customtkinter.CTkLabel(master=menuFrame, text="\nLinux Extraction System", font=("Helvetica", 20))
 subTitle.pack()
+
+###
+
+exitFrame = customtkinter.CTkFrame(master=menuFrame)
+exitFrame.pack(padx=1, pady=5, side="right")
+exitButton = customtkinter.CTkButton(master=exitFrame, text="Exit", command=lambda: [closeReport(), app.destroy()])
+exitButton.pack(padx=10, pady=10)
 
 app.mainloop()
